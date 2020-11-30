d168befbfe354b42a04300881f5a4b7196e571b3
grabania <grabania.work@gmail.com>
2020-11-30 15:07:06 +0000

    Add Container & change Nav className in Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,15 +4,17 @@ import { Navbar, Nav, Container } from "react-bootstrap"
 const Header = () => {
   return (
     <header>
-      <Navbar bg='dark' variant="dark" expand='lg' collapseOnSelect>
-        <Navbar.Brand href='/'>Proshop</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='mr-auto'>
-            <Nav.Link href='/cart'>Cart</Nav.Link>
-            <Nav.Link href='/login'>Sign in</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>Proshop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/cart'>Cart</Nav.Link>
+              <Nav.Link href='/login'>Sign in</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </header>
   )
